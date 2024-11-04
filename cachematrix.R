d641bfb9fa639c0b7a8eb8dd45f4beabdda35b2a
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2024-11-04 18:45:56 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,30 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function() {
+        mat<-NULL
+        inv_cache<-NULL
 
+        setMatrix<-function(x){
+                mat<<-x
+                inv_cache<<- NULL
 }
-
+        getInverse<-function(x){
+                mat <<- x
+                inv_cache<<-NULL
+                }
+        getInverse<-function(){
+                if(is.null(inv_cache)){
+                        inv_cache<<-solve(mat)
+                        }
+                inv_cache
+                }
+        list(setMatrix=setMatrix,getInverse=getInverse)
+        }
+cacheSolve<-function(x,...){
+        inv_cache<-x$getInverse()
+        inv_cache
+        }
 
 ## Write a short comment describing this function
 
